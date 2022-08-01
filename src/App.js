f33889eefd421da9090a230cea8d2029160c5c11
AmadeusDias <amadeusdias@hotmail.com>
2022-08-01 20:46:56 -0300

    EsLint correction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Album from './pages/Album';
 import Search from './pages/Search';
