cc41da69c79e8078574d913fa0ba0a5130af86d2
AmadeusDias <amadeusdias@hotmail.com>
2022-08-01 20:07:42 -0300

    requisito 1, rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import NotFound from './pages/NotFound';
+import ProfileEdit from './pages/ProfileEdit';
+import Profile from './pages/Profile';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!</p>);
+    return (
+      <div>
+        <p>TrybeTunes!</p>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/*" component={ NotFound } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+      </div>
+    );
   }
 }
 
