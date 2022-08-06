5b748ad1ae8fe60af3e705ceca720b3433f2efcf
AmadeusDias <amadeusdias@hotmail.com>
2022-08-05 21:12:17 -0300

    Renderização dos albums

---
+++ src/components/MusicCard.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MusicCard extends React.Component {
+  render() {
+    const { trackName, previewUrl } = this.props;
+    return (
+      <span>
+        <p>{trackName}</p>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+      </span>
+    );
+  }
+}
+
+export default MusicCard;
+
+MusicCard.propTypes = {
+  trackName: PropTypes.string.isRequired,
+  previewUrl: PropTypes.string.isRequired,
+
+};
--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,16 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
+import MusicCard from '../components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
 class Album extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      musics: [],
+      artistName: '',
+      artistAlbum: '',
+    };
+  }
+
+  async componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    const response = await getMusics(id);
+    this.setState({
+      musics: response,
+      artistName: response[0].artistName,
+      artistAlbum: response[0].collectionName,
+    });
+  }
+
   render() {
+    const { musics, artistAlbum, artistName } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
-        Album
+        <h4 data-testid="artist-name">{artistName}</h4>
+        <h4 data-testid="album-name">{artistAlbum}</h4>
+        {musics.map((musica, index) => index !== 0 && (
+          <MusicCard
+            key={ index }
+            trackName={ musica.trackName }
+            previewUrl={ musica.previewUrl }
+          />
+        ))}
 
       </div>
     );
   }
 }
+
+// a linha 34 possui um condicional, assim como o utilizado na page Search: se o index do elemento musica for diferente de zero ele pode ser renderizado, se o index for igual a zero ele é considerado falsy e não é renderizado. Isso é necessario pq o primeiro elemento do array musics so possui informações sobre o albúm, não tem a previewUrl que faz a musica tocar, oq tava gerando uma faixa morta na renderização do albúm.
+
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default Album;
